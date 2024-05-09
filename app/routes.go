728094891d7dc5e16643d86990289e5b50215362
albanybuipe96 <mohammedarahman96@gmail.com>
2024-05-09 21:54:55 +0000

    added patch and delete routes

--- app/routes.go
+++ app/routes.go
@@ -8,8 +8,13 @@ func appRoutes() {
 	router.GET("/ping", handlers.Ping)
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/users/:user_id", handlers.GetUser)
 		v1.POST("/users/", handlers.CreateUser)
+
+		v1.GET("/users/:user_id", handlers.GetUser)
 		v1.GET("/users/", handlers.GetUsers)
+
+		v1.PATCH("/users/:user_id", handlers.UpdateUser)
+
+		v1.DELETE("/users/:user_id", handlers.DeleteUser)
 	}
 }
