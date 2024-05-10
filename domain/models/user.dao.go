56318fc8093d02896275d5ee76c0a3acefe011f9
albanybuipe96 <mohammedarahman96@gmail.com>
2024-05-10 14:58:03 +0000

    updated CRUD methods on User for postgres

--- domain/models/user.dao.go
+++ domain/models/user.dao.go
@@ -3,56 +3,44 @@ package models
 import (
 	"log"
 
-	"github.com/albanybuipe96/bookstore-users-api/data/mysql/datasource"
-	"github.com/albanybuipe96/bookstore-users-api/data/mysql/queries"
+	"github.com/albanybuipe96/bookstore-users-api/data/postgres/datasource"
+	"github.com/albanybuipe96/bookstore-users-api/data/postgres/queries"
 	"github.com/albanybuipe96/bookstore-users-api/utils/errors"
 )
 
 // Save saves a user to the database.
 // It prepares an insert statement, executes it with the user's details, and returns an error if any step fails.
 func (user *User) Save() *errors.CustomError {
-	query := queries.Query{TableName: "users", DbEngine: queries.MySQL}
-	statement, err := datasource.DbClient.Prepare(query.Insert())
-
-	if err != nil {
-		log.Println(err.Error())
-		return errors.InternalServerError(err.Error())
-	}
-	defer statement.Close()
-
-	result, err := statement.Exec(
+	query := queries.Query{TableName: "users", DbEngine: queries.PostgreSQL}
+	err := datasource.DbClient.QueryRow(
+		query.Insert(),
 		user.FirstName,
 		user.LastName,
 		user.Email,
 		user.Status,
 		user.Password,
 		user.CreatedAt,
+	).Scan(
+		&user.Id,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Status,
+		&user.Password,
+		&user.CreatedAt,
 	)
 	if err != nil {
+		log.Println("ERROR", err)
 		return errors.ReportDbError(err)
 	}
-
-	id, err := result.LastInsertId()
-	if err != nil {
-		log.Println(err.Error())
-		return errors.InternalServerError(err.Error())
-	}
-
-	user.Id = id
 
 	return nil
 }
 
 func (user *User) Update() *errors.CustomError {
-	query := queries.Query{TableName: "users", DbEngine: queries.MySQL}
-	statement, err := datasource.DbClient.Prepare(query.Update())
-	if err != nil {
-		log.Println(err.Error())
-		return errors.ReportDbError(err)
-	}
-	defer statement.Close()
-
-	_, err = statement.Exec(
+	query := queries.Query{TableName: "users", DbEngine: queries.PostgreSQL}
+	_, err := datasource.DbClient.Exec(
+		query.Update(),
 		user.FirstName,
 		user.LastName,
 		user.Email,
@@ -65,6 +53,7 @@ func (user *User) Update() *errors.CustomError {
 		log.Println(err.Error())
 		return errors.ReportDbError(err)
 	}
+
 	return nil
 }
 
@@ -74,7 +63,6 @@ func (user *User) Get() *errors.CustomError {
 	query := queries.Query{TableName: "users", DbEngine: queries.MySQL}
 	statement, err := datasource.DbClient.Prepare(query.Fetch())
 	if err != nil {
-
 		return errors.ReportDbError(err)
 	}
 	defer statement.Close()
@@ -86,9 +74,9 @@ func (user *User) Get() *errors.CustomError {
 		&user.FirstName,
 		&user.LastName,
 		&user.Email,
-		&user.CreatedAt,
 		&user.Status,
 		&user.Password,
+		&user.CreatedAt,
 	); err != nil {
 		log.Println(err.Error())
 		return errors.ReportDbError(err)
@@ -100,7 +88,7 @@ func (user *User) Get() *errors.CustomError {
 // GetAllUsers retrieves all users from the database.
 // It prepares a fetch all statement, executes it, and returns a list of users or an error if any step fails.
 func (user *User) GetAllUsers() ([]*User, *errors.CustomError) {
-	query := queries.Query{TableName: "users", DbEngine: queries.MySQL}
+	query := queries.Query{TableName: "users"}
 	statement, err := datasource.DbClient.Prepare(query.FetchAll())
 	if err != nil {
 		log.Println(err.Error())
@@ -128,9 +116,9 @@ func (user *User) GetAllUsers() ([]*User, *errors.CustomError) {
 			&user.FirstName,
 			&user.LastName,
 			&user.Email,
-			&user.CreatedAt,
 			&user.Status,
 			&user.Password,
+			&user.CreatedAt,
 		)
 		if err != nil {
 			log.Println(err.Error())
@@ -143,7 +131,7 @@ func (user *User) GetAllUsers() ([]*User, *errors.CustomError) {
 }
 
 func (user *User) GetUserByStatus(status string) ([]*User, *errors.CustomError) {
-	query := queries.Query{TableName: "users", DbEngine: queries.MySQL}
+	query := queries.Query{TableName: "users", DbEngine: queries.PostgreSQL}
 	statement, err := datasource.DbClient.Prepare(query.FetchByStatus())
 	if err != nil {
 		log.Println(err.Error())
@@ -170,9 +158,9 @@ func (user *User) GetUserByStatus(status string) ([]*User, *errors.CustomError) 
 			&user.FirstName,
 			&user.LastName,
 			&user.Email,
-			&user.CreatedAt,
 			&user.Status,
 			&user.Password,
+			&user.CreatedAt,
 		)
 		if err != nil {
 			log.Println(err.Error())
@@ -181,23 +169,13 @@ func (user *User) GetUserByStatus(status string) ([]*User, *errors.CustomError) 
 		results = append(results, &user)
 	}
 
-	//if len(results) == 0 {
-	//	return nil, errors.NotFoundError("no match found for given status")
-	//}
-
 	return results, nil
 }
 
 func (user *User) Delete() *errors.CustomError {
-	query := queries.Query{TableName: "users", DbEngine: queries.MySQL}
-	statement, err := datasource.DbClient.Prepare(query.Delete())
+	query := queries.Query{TableName: "users", DbEngine: queries.PostgreSQL}
+	_, err := datasource.DbClient.Exec(query.Delete(), user.Id)
 	if err != nil {
-		log.Println(err.Error())
-		return errors.ReportDbError(err)
-	}
-	defer statement.Close()
-
-	if _, err = statement.Exec(user.Id); err != nil {
 		log.Println(err.Error())
 		return errors.ReportDbError(err)
 	}
